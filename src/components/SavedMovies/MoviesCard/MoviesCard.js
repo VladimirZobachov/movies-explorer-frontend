63fa87a26a6f66e237165f733179a6100ae1be1b
Vladimir <zobachov@gmail.com>
2022-12-07 19:56:08 +0500

    исправлено: Блок карточки на странице «Сохранённые фильмы» должен содержать кнопку удаления, а не лайка

--- src/components/SavedMovies/MoviesCard/MoviesCard.js
+++ src/components/SavedMovies/MoviesCard/MoviesCard.js
@@ -8,7 +8,7 @@ function MoviesCard(props) {
         <li className="movies__card">
             <div className="movies__card-header">
                 <h3 className="movies__card-title">{props.card.nameRU}</h3>
-                <button type="button" className="movies__card-save movies__card-save-action"
+                <button type="button" className="movies__card-save movies__card-save_del"
                  onClick={handleCardDel}/>
                 <span className="movies__card-duration">{props.card.duration}</span>
             </div>
