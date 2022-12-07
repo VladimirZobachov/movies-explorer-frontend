9fdcbeccc4ebc15a105f604423c08fef8bc81852
Vladimir <zobachov@gmail.com>
2022-12-07 21:37:44 +0500

    исправлено: Вместо тега "а", как сделано сейчас, должны использоваться компоненты Link

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
+import {Link} from "react-router-dom";
 
 function NavTab() {
   return (
     <header className="header">
       <ul className="header__navigation">
-        <li className="header__navigation-item"><a href="/"><img src={logo} alt="лого" /></a></li>
+        <li className="header__navigation-item"><Link to="/"><img src={logo} alt="лого" /></Link></li>
         <li className="header__navigation-item header__navigation-item_button-register">
-          <a href="/signup">Регистрация</a>
+          <Link to="/signup">Регистрация</Link>
         </li>
         <li className="header__navigation-item ">
-          <a href="/signin" className="header__navigation-button-signin">Войти</a>
+          <Link to="/signin" className="header__navigation-button-signin">Войти</Link>
         </li>
       </ul>
     </header>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 import IconMenu from '../../images/icon-menu.svg';
 import logo from '../../images/logo.svg';
 
@@ -11,16 +11,16 @@ function Navigation({handleOpenPopup}) {
         <header className="header">
           <ul className="header__navigation-movies">
             <li className="header__navigation-item header__navigation-item_logo">
-              <a href="/"><img src={logo} alt="лого" /></a>
+              <Link to="/"><img src={logo} alt="лого" /></Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/movies" className="header__navigation-item-link">Фильмы</a>
+              <Link to="/movies" className="header__navigation-item-link">Фильмы</Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</a>
+              <Link to="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/profile" className="header__navigation-button-profile">Аккаунт</a>
+              <Link to="/profile" className="header__navigation-button-profile">Аккаунт</Link>
             </li>
             <li className="header__navigation-item header__navigation-button-menu">
               <button className="header__navigation-button-menu" type="button" onClick={handleOpenPopup}>
@@ -34,16 +34,16 @@ function Navigation({handleOpenPopup}) {
         <header className="header">
           <ul className="header__navigation-movies">
             <li className="header__navigation-item header__navigation-item_logo">
-              <a href="/"><img src={logo} alt="лого" /></a>
+              <Link to="/"><img src={logo} alt="лого" /></Link>
             </li>
             <li className="header__navigation-item">
-              <b><a href="/movies" className="header__navigation-item-link">Фильмы</a></b>
+              <b><Link to="/movies" className="header__navigation-item-link">Фильмы</Link></b>
             </li>
             <li className="header__navigation-item">
-              <a href="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</a>
+              <Link to="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/profile" className="header__navigation-button-profile">Аккаунт</a>
+              <Link to="/profile" className="header__navigation-button-profile">Аккаунт</Link>
             </li>
             <li className="header__navigation-item header__navigation-button-menu">
               <button className="header__navigation-button-menu" type="button" onClick={handleOpenPopup}>
@@ -57,16 +57,16 @@ function Navigation({handleOpenPopup}) {
         <header className="header">
           <ul className="header__navigation-movies">
             <li className="header__navigation-item header__navigation-item_logo">
-              <a href="/"><img src={logo} alt="лого" /></a>
+              <Link to="/"><img src={logo} alt="лого" /></Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/movies" className="header__navigation-item-link">Фильмы</a>
+              <Link to="/movies" className="header__navigation-item-link">Фильмы</Link>
             </li>
             <li className="header__navigation-item">
-              <b><a href="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</a></b>
+              <b><Link to="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</Link></b>
             </li>
             <li className="header__navigation-item">
-              <a href="/profile" className="header__navigation-button-profile">Аккаунт</a>
+              <Link to="/profile" className="header__navigation-button-profile">Аккаунт</Link>
             </li>
             <li className="header__navigation-item header__navigation-button-menu">
               <button className="header__navigation-button-menu" type="button" onClick={handleOpenPopup}>
@@ -80,16 +80,16 @@ function Navigation({handleOpenPopup}) {
         <header className="header">
           <ul className="header__navigation-movies">
             <li className="header__navigation-item header__navigation-item_logo">
-              <a href="/"><img src={logo} alt="лого" /></a>
+              <Link to="/"><img src={logo} alt="лого" /></Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/movies" className="header__navigation-item-link">Фильмы</a>
+              <Link to="/movies" className="header__navigation-item-link">Фильмы</Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</a>
+              <Link to="/saved-movies" className="header__navigation-item-link">Сохраненные фильмы</Link>
             </li>
             <li className="header__navigation-item">
-              <a href="/profile" className="header__navigation-button-profile">Аккаунт</a>
+              <Link to="/profile" className="header__navigation-button-profile">Аккаунт</Link>
             </li>
             <li className="header__navigation-item header__navigation-button-menu">
               <button className="header__navigation-button-menu" type="button" onClick={handleOpenPopup}>
